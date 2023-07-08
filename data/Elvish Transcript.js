08dbc52c83896f8123ec0e6c000963aa9f472cfd
Ika <ikatyang@gmail.com>
2023-07-08 19:00:17 +0800

    build: mark `data` and `lib` as gitignored

--- data/Elvish Transcript.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "name": "Elvish Transcript",
-  "type": "programming",
-  "group": "Elvish",
-  "aceMode": "text",
-  "tmScope": "source.elvish-transcript",
-  "color": "#55BB55",
-  "languageId": 452025714
-}
